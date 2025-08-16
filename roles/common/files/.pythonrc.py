9e88a11847ccedcf0bca1d18730c0dc1233648b4
Leo-WJ15 <LeoJhon.Song@outlook.com>
2025-08-17 03:55:54 +0800

    update python prompt configuration

--- roles/common/files/.pythonrc.py
+++ roles/common/files/.pythonrc.py
@@ -5,51 +5,57 @@ import os
 import re
 import time
 
+def wrap_ansi(text):
+    """Wrap ANSI escape sequences with readline markers to make them invisible for length calculation"""
+    return '\001' + text + '\002'
 
-def __len__(str):
-    return 20
-
-# colors
-OFFSET = '\033[0m'
-BLACK_FG = '\033[30m'
-RED_FG = '\033[31m'
-GREEN_FG = '\033[32m'
-YELLOW_FG = '\033[33m'
-BLUE_FG = '\033[34m'
-PURPLE_FG = '\033[35m'
-CYAN_FG = '\033[36m'
-WHITE_FG = '\033[37m'
-BLACK_BG = '\033[40m'
-RED_BG = '\033[41m'
-GREEN_BG = '\033[42m'
-YELLOW_BG = '\033[43m'
-BLUE_BG = '\033[44m'
-PURPLE_BG = '\033[45m'
-CYAN_BG = '\033[46m'
-WHITE_BG = '\033[47m'
+# colors with readline markers
+OFFSET = wrap_ansi('\033[0m')
+BLACK_FG = wrap_ansi('\033[30m')
+RED_FG = wrap_ansi('\033[31m')
+GREEN_FG = wrap_ansi('\033[32m')
+YELLOW_FG = wrap_ansi('\033[33m')
+BLUE_FG = wrap_ansi('\033[34m')
+PURPLE_FG = wrap_ansi('\033[35m')
+CYAN_FG = wrap_ansi('\033[36m')
+WHITE_FG = wrap_ansi('\033[37m')
+BLACK_BG = wrap_ansi('\033[40m')
+RED_BG = wrap_ansi('\033[41m')
+GREEN_BG = wrap_ansi('\033[42m')
+YELLOW_BG = wrap_ansi('\033[43m')
+BLUE_BG = wrap_ansi('\033[44m')
+PURPLE_BG = wrap_ansi('\033[45m')
+CYAN_BG = wrap_ansi('\033[46m')
+WHITE_BG = wrap_ansi('\033[47m')
 
 # some segments you may want to use
-python_version = ' Python ' + ".".join(map(str, sys.version_info[:3]))
-python_interpreter_path = '  ' + sys.executable
-local_time = ' ' + time.strftime('%H:%M:%S', time.localtime(time.time()))
+python_version = '  ' + ".".join(map(str, sys.version_info[:3]))
+python_interpreter_path = ' 󰞷 ' + sys.executable
+local_time = ' 󱑌 ' + time.strftime('%H:%M:%S', time.localtime(time.time()))
 # the delimiter used at the end of a segment
 delimiter = ''
-pointer = ' '
+
 # segments of the prompt
-prompt = [
+prompt_segments = [
     [BLUE_BG, WHITE_FG, python_version],
     [GREEN_BG, WHITE_FG, python_interpreter_path],
     [WHITE_BG, BLACK_FG, local_time]
 ]
 
-sys.ps1 = OFFSET
-for i in range(len(prompt)):
-    sys.ps1 = sys.ps1 + prompt[i][0] + prompt[i][1] + ' ' + prompt[i][2]
-    sys.ps1 = sys.ps1 + ' ' + OFFSET + re.sub(r'(?<=\033\[)\d', '3', prompt[i][0])
-    if i == len(prompt)-1:
-        sys.ps1 = sys.ps1 + delimiter
-    else:
-        sys.ps1 = sys.ps1 + prompt[i+1][0] + delimiter
-    sys.ps1 = sys.ps1 + OFFSET
+# Build the prompt
+sys.ps1 = ''
+for i, (bg_color, fg_color, text) in enumerate(prompt_segments):
+    # Add current segment
+    sys.ps1 += bg_color + fg_color + text + ' '
 
-sys.ps1 = sys.ps1 + '\n' + pointer
+    # Add transition to next segment or end
+    if i < len(prompt_segments) - 1:
+        # Get next segment's background color for transition
+        next_bg = prompt_segments[i + 1][0]
+        # Convert current background to foreground for the arrow
+        current_bg_as_fg = wrap_ansi(re.sub(r'4(\d)', r'3\1', bg_color.strip('\001\002')))
+        sys.ps1 += OFFSET + next_bg + current_bg_as_fg + delimiter
+    else:
+        # Last segment - transition to normal background
+        current_bg_as_fg = wrap_ansi(re.sub(r'4(\d)', r'3\1', bg_color.strip('\001\002')))
+        sys.ps1 += OFFSET + current_bg_as_fg + delimiter + OFFSET
